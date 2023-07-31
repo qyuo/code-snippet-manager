63805ac8f2c7a5c9edb2aa7e678e0600da33746a
Luis <ale.pabon04@gmail.com>
2023-07-31 16:30:48 -0500

    Fixed last bugs

--- client/src/components/snippet-manager/Navbar.js
+++ client/src/components/snippet-manager/Navbar.js
@@ -34,10 +34,10 @@ const Navbar = () => {
               viewBox="0 0 20 24"
               fill="none"
               stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="feather feather-log-out"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-log-out"
             >
               <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
               <polyline points="16 17 21 12 16 7"></polyline>
